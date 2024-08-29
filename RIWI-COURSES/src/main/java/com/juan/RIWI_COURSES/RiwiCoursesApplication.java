cfa1ace16bada221f36de679484c4da08322300c
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-08-29 17:17:11 -0500

    project

---
+++ RIWI-COURSES/src/main/java/com/juan/RIWI_COURSES/RiwiCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.juan.RIWI_COURSES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiwiCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiwiCoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juan/RIWI_COURSES/RiwiCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.RIWI_COURSES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiwiCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
