294e8b046e51f6d5b41abc2b38c72e324d43ead5
andres1988 <a.canorave@gmail.com>
2024-08-30 18:16:30 -0500

    add response student and class

--- src/main/java/com/juan/RIWI_COURSES/dtos/response/StudentResponse.java
+++ src/main/java/com/juan/RIWI_COURSES/dtos/response/StudentResponse.java
@@ -1,4 +1,25 @@
 package com.juan.RIWI_COURSES.dtos.response;
 
+import com.juan.RIWI_COURSES.entities.ClassEntity;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class StudentResponse {
+
+    private String name;
+    private String email;
+    private String clase;
+    private ClassEntity class_id;
+    private List<ClassEntity> classes;
+
 }
