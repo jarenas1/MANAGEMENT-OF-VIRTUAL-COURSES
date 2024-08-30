96fe31c33e91e88af37624471cc1d08fc71e7421
Santiago <santimh04@gmail.com>
2024-08-30 16:08:36 -0500

    feature: filled Controllers structure

---
+++ src/main/java/com/juan/RIWI_COURSES/controllers/LessonController.java
@@ -0,0 +1,14 @@
+package com.juan.RIWI_COURSES.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class LessonController {
+    //Atributos de LessonController
+    //Constructores de LessonController
+    //Asignadores de atributos de LessonController (setters)
+    //Lectores de atributos de LessonController (getters)
+    //Métodos de LessonController
+}
