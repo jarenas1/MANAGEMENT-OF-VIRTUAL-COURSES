2f8afd039b55aa2883451bec08948360e3b36270
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-08-30 15:14:16 -0500

    create/TableNameModified

--- src/main/java/com/juan/RIWI_COURSES/entities/LessonEntity.java
+++ src/main/java/com/juan/RIWI_COURSES/entities/LessonEntity.java
@@ -1,16 +1,20 @@
 package com.juan.RIWI_COURSES.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
+//Lombok called
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "lessons")
 public class LessonEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private boolean status;
     private String name;
--- src/main/java/com/juan/RIWI_COURSES/entities/MultimediaEntity.java
+++ src/main/java/com/juan/RIWI_COURSES/entities/MultimediaEntity.java
@@ -2,14 +2,17 @@ package com.juan.RIWI_COURSES.entities;
 
 import com.juan.RIWI_COURSES.utils.Type;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Entity
+//Lombok called
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "multimedia")
 public class MultimediaEntity {
 
     private String id;
