d6be325dc79a767e8b101281e4495ee8fee65211
Santiago <santimh04@gmail.com>
2024-08-30 18:57:52 -0500

    feature: StudentRepository file created

--- src/main/java/com/juan/RIWI_COURSES/repositories/CRUDRepository.java
+++
@@ -1,9 +0,0 @@
-package com.juan.RIWI_COURSES.repositories;
-
-public interface CRUDRepository {
-    //Atributos de CRUDRepository
-    //Constructores de CRUDRepository
-    //Asignadores de atributos de CRUDRepository (setters)
-    //Lectores de atributos de CRUDRepository (getters)
-    //Métodos de CRUDRepository
-}
