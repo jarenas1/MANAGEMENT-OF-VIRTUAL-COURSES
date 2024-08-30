cc3c65bb9363b6f4bf77382f9eb2216dc7a0faee
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-08-30 15:04:30 -0500

    create/MultimediaAtributes

--- src/main/java/com/juan/RIWI_COURSES/entities/MultimediaEntity.java
+++ src/main/java/com/juan/RIWI_COURSES/entities/MultimediaEntity.java
@@ -1,4 +1,13 @@
 package com.juan.RIWI_COURSES.entities;
 
+import com.juan.RIWI_COURSES.utils.Type;
+
 public class MultimediaEntity {
+
+    private String id;
+    private Type type;
+    private LessonEntity lesson_id;
+    private boolean status;
+    private String url;
+
 }
