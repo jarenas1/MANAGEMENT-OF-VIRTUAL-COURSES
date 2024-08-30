57fd4ec2a31b741fbb9e8ae7c2d9485cf96d3d2b
andres1988 <a.canorave@gmail.com>
2024-08-30 15:40:58 -0500

    feature:create entities student and class

--- src/main/java/com/juan/RIWI_COURSES/entities/ClassEntity.java
+++ src/main/java/com/juan/RIWI_COURSES/entities/ClassEntity.java
@@ -1,4 +1,46 @@
 package com.juan.RIWI_COURSES.entities;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.core.SpringVersion;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Entity
+@Table(name = "classes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ClassEntity {
+
+    @Id //Anotación para que sea valor unico o primary key
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column (nullable = false,length = 155)
+    private String name;
+    @Column (nullable = false,length = 155)
+    private String description;
+    @Column (nullable = false)
+    private LocalDate createAt;
+    @Column(nullable = false)
+    private Boolean status;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(mappedBy = "class", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private List<StudentEntity> students;
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/com/juan/RIWI_COURSES/entities/StudentEntity.java
+++ src/main/java/com/juan/RIWI_COURSES/entities/StudentEntity.java
@@ -1,4 +1,23 @@
 package com.juan.RIWI_COURSES.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
 public class StudentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @Column
+
+
+
 }
