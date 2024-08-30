c00538f8e89467d4f601a1574084eb07bc097e20
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-08-30 17:54:58 -0500

    Lesson/MultimediaDTOS

--- src/main/java/com/juan/RIWI_COURSES/dtos/response/ClassResponse.java
+++ src/main/java/com/juan/RIWI_COURSES/dtos/response/ClassResponse.java
@@ -1,4 +1,17 @@
 package com.juan.RIWI_COURSES.dtos.response;
 
+import com.juan.RIWI_COURSES.utils.Type;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClassResponse {
+    private Type type;
+    private String lesson_id;
+    private String url;
 }
--- src/main/java/com/juan/RIWI_COURSES/dtos/response/MultimediaResponse.java
+++ src/main/java/com/juan/RIWI_COURSES/dtos/response/MultimediaResponse.java
@@ -1,4 +1,8 @@
 package com.juan.RIWI_COURSES.dtos.response;
 
+
 public class MultimediaResponse {
+
+    //Media without id and status
+
 }
