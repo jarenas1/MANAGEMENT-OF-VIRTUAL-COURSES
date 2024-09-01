8de19f755c71c2c80e15b35c2f7b64a77312335a
andres1988 <a.canorave@gmail.com>
2024-09-01 11:14:51 -0500

    configuration swagger

---
+++ src/main/java/com/juan/RIWI_COURSES/config/Swagger.java
@@ -0,0 +1,15 @@
+package com.juan.RIWI_COURSES.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@Configuration
+@OpenAPIDefinition()
+
+
+public class Swagger {
+}
