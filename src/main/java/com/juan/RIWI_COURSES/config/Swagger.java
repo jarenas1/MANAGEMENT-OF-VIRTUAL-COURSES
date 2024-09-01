642e8c4b8cb848dda7d18eb94f8823b670b6b3c6
andres1988 <a.canorave@gmail.com>
2024-09-01 11:23:09 -0500

    modification swagger

--- src/main/java/com/juan/RIWI_COURSES/config/Swagger.java
+++ src/main/java/com/juan/RIWI_COURSES/config/Swagger.java
@@ -5,10 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
+
 
 @Configuration
-@OpenAPIDefinition()
+@OpenAPIDefinition(info = @Info(contact = @Contact(name = "Andres, Santiago,Juan", url = "https://github.com/jarenas1/MANAGEMENT-OF-VIRTUAL-COURSES"), title = "Documentation: MANAGEMENT-OF-VIRTUAL-COURSES ", version = "1.0.0"))
+
 
 
 public class Swagger {
