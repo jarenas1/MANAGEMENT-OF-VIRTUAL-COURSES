e9aa12e8a05c3b22d973e2d18513c07987c7c1f9
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-09-01 10:29:34 -0500

    fixed

--- src/main/java/com/juan/RIWI_COURSES/controllers/LessonController.java
+++ src/main/java/com/juan/RIWI_COURSES/controllers/LessonController.java
@@ -1,14 +1,23 @@
 package com.juan.RIWI_COURSES.controllers;
 
+import com.juan.RIWI_COURSES.dtos.request.LessonRequest;
+import com.juan.RIWI_COURSES.services.interfaces.LessonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/api/vi/")
 public class LessonController {
-    //Atributos de LessonController
-    //Constructores de LessonController
-    //Asignadores de atributos de LessonController (setters)
-    //Lectores de atributos de LessonController (getters)
-    //Métodos de LessonController
+    @Autowired
+    LessonService lessonService;
+
+    @PostMapping("lesson")
+    public ResponseEntity<LessonRequest>save(@RequestBody LessonRequest lessonRequest){
+
+    }
+
 }
